4b9ba449c7745acc6133fd834e7d715055c710cd
root <root@imagine-create-thrive.localdomain>
2017-11-01 17:57:03 +0000

    ng-bs-native-girl

--- ng-bs-native-girls/e2e/app.po.ts
+++ ng-bs-native-girls/e2e/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, element, by } from 'protractor';
+import { browser, by, element } from 'protractor';
 
-export class AngularBootstrapPage {
+export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
