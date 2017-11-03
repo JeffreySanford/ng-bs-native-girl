769b2cbd7c312152203ca2255fccbd86a660aa06
Jeffrey Sanford <jeffreysanford@gmail.com>
2017-11-03 15:31:26 -0600

    Angular 4 and Bootsrap.  Added sub pages.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('ng-bs-native-girls App', () => {
+describe('ng-bs-gratitude-girls App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavigationComponent } from './header/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule
